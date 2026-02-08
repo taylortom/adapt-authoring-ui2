698c3dbe330433df5036925f3b580fb7c6776878
Thomas Taylor <thomas.taylor@kineo.com>
2026-02-08 00:23:58 +0000

    Update files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+/**
+ * Note: this config is for CI use only to confirm build
+ */
 export default defineConfig({
   plugins: [react()],
   server: {
