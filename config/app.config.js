3137325a357a94a0244941e3e5ceba1480ee695e
Thomas Taylor <thomas.taylor@kineo.com>
2026-02-08 00:26:49 +0000

    Remove unused config

--- config/app.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiUrl: 'http://localhost:3001/api',
-  wsUrl: 'ws://localhost:3001',
-  appName: 'My App',
-  features: {
-    enableWebSockets: true,
-    enableCharts: true
-  }
-}
