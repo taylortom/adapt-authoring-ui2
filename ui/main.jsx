31f5d64b38f9ea57f09bd62b966a19ea7119d903
Thomas Taylor <thomas.taylor@kineo.com>
2026-02-10 10:53:36 +0000

    Set theme mode

--- ui/main.jsx
+++ ui/main.jsx
@@ -23,7 +23,7 @@ loadConfig().then(() => {
   ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme} defaultMode='system'>
           <CssBaseline />
           <App />
           <ReactQueryDevtools initialIsOpen={false} />
