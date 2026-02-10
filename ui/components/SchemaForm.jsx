a9db18aeb38fefd94d19daf825a9951d2bb1f046
Thomas Taylor <thomas.taylor@kineo.com>
2026-02-10 12:33:31 +0000

    Add schema blacklist

--- ui/components/SchemaForm.jsx
+++ ui/components/SchemaForm.jsx
@@ -3,6 +3,35 @@ import { useQuery } from '@tanstack/react-query'
 import Form from '@rjsf/mui'
 import validator from '@rjsf/validator-ajv8'
 
+const ATTRIBUTE_BLACKLIST = [
+  '_colorLabel',
+  '_component',
+  '_componentType',
+  '_courseId',
+  '_enabledPlugins',
+  '_hasPreview',
+  '_id',
+  '_isSelected',
+  '_latestTrackingId',
+  '_layout',
+  '_menu',
+  '_parentId',
+  '_sortOrder',
+  '_supportedLayout',
+  '_theme',
+  '_themePreset',
+  '_trackingId',
+  '_type',
+  'createdAt',
+  'createdBy',
+  'layoutOptions',
+  'menuSettings',
+  'themeSettings',
+  'themeVariables',
+  'updatedAt',
+  'userGroups'
+]
+
 const SchemaForm = ({ apiName, uiSchema, dataId, queryString, disableCache = false, onSubmit }) => {
   const api = useApi(apiName)
   const cacheOpts = disableCache ? { gcTime: 0, staleTime: 0 } : {}
@@ -23,6 +52,8 @@ const SchemaForm = ({ apiName, uiSchema, dataId, queryString, disableCache = fal
   if (!schema || !data) {
     return ''
   }
+  ATTRIBUTE_BLACKLIST.forEach(a => delete schema.properties[a])
+
   return (
     <Form
       schema={schema}
